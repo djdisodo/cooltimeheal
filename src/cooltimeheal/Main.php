969ed93937379c60fe631140553cdcb022005dbf
djdisodo <jisoohero30@gmail.com>
2017-05-08 23:51:27 +0900

    Add files via upload

---
+++ src/cooltimeheal/Main.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace cooltimeheal;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+class Main extends PluginBase implements Listener {
+	public function onEnable() {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	public function onCommand(CommandSender $Sender,Command $command, $label,array $args) {
+		if ($command->getName() == '힐') {
+			$playername = $Sender->getName();
+			$playername = strtolower( $playername );
+			$cooltime = 10; //10초
+			$playername = base_convert( $playername, 35, 10);
+			$shmid = shmop_open($playername, 'c', 0755, 1024);
+			if ( shmop_read( $shmid, 0, 11 ) <= time() ) {
+				$Sender->sendMessage(TextFormat::RED, "회복되었습니다");
+				$Sender->heal($player->getMaxHealth(), new EntityRegainHealthEvent($player, $player->getMaxHealth() - $player->getHealth(), EntityRegainHealthEvent::CAUSE_CUSTOM));
+				shmop_write($shmid, time() + $cooltime, 0);
+				return true;
+			} else {
+				$cl = shmop_read( $shmid, 0, 11 ) - time();
+				$Sender->sendMessage(TextFormat::RED, '쿨타임'.$cl.'초가 남았습니다');
+				return true;
+			}
+		}
+	}
+}
+?>
